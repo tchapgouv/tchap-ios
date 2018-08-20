8d6d6f1945d37178256ebc0e2c3536d62f260402
SBiOSoftWhare <steveb@matrix.org>
2018-08-20 14:07:20 +0200

    Create RoomsCoordinator to handle rooms navigation flow.

---
+++ Tchap/Modules/Rooms/RoomsCoordinator.swift
@@ -0,0 +1,42 @@
+/*
+ Copyright 2018 New Vector Ltd
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+import UIKit
+
+final class RoomsCoordinator: RoomsCoordinatorType {
+    
+    private let roomsViewController: RoomsViewController
+    private let session: MXSession
+    private let recentsDataSource: RecentsDataSource
+    
+    var childCoordinators: [Coordinator] = []
+    
+    init(session: MXSession) {
+        self.session = session
+        
+        self.roomsViewController = RoomsViewController.instantiate()
+        self.recentsDataSource = RecentsDataSource(matrixSession: self.session)
+        self.recentsDataSource.setDelegate(self.roomsViewController, andRecentsDataSourceMode: RecentsDataSourceModeRooms)
+    }
+    
+    func start() {
+        self.roomsViewController.displayList(self.recentsDataSource)
+    }
+    
+    func toPresentable() -> UIViewController {
+        return self.roomsViewController
+    }
+}
