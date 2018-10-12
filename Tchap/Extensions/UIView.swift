539ea425a5fd4825961d0e202b92d5feb1881656
SBiOSoftWhare <steveb@matrix.org>
2018-10-12 11:00:52 +0200

    Add convenient method on UIView to make an hexagon with border

--- Tchap/Extensions/UIView.swift
+++ Tchap/Extensions/UIView.swift
@@ -31,4 +31,77 @@ extension UIView {
             constraints.forEach { $0.isActive = true }
         }
     }
+    
+    func tc_mask(withPath path: UIBezierPath, inverse: Bool = false) {
+        let path = path
+        let maskLayer = CAShapeLayer()
+        
+        if inverse {
+            path.append(UIBezierPath(rect: self.bounds))
+            maskLayer.fillRule = kCAFillRuleEvenOdd
+        }
+        
+        maskLayer.path = path.cgPath
+        
+        self.layer.mask = maskLayer
+    }
+    
+    func tc_makeCircle() {
+        let minDimension = min(self.bounds.size.width, self.bounds.size.height)
+        self.layer.cornerRadius = minDimension/2
+        self.layer.masksToBounds = true
+    }
+    
+    func tc_makeHexagon(borderWidth: CGFloat = 0.0, borderColor: UIColor = UIColor.black) {
+        let path = UIBezierPath(polygonIn: self.bounds, sides: 6)
+        self.tc_mask(withPath: path)
+        
+        self.tc_addOrUpdateBorderLayer(with: path.cgPath, name: "HexagonBorderLayer", bringToFront: true, borderWidth: borderWidth, borderColor: borderColor)
+    }
+    
+    func tc_addOrUpdateBorderLayer(with path: CGPath, name: String, bringToFront: Bool = true, borderWidth: CGFloat = 0.0, borderColor: UIColor = UIColor.black) {
+        
+        let foundBorderLayerIndex = self.layer.sublayers?.index(where: { sublayer -> Bool in
+            return sublayer.name == name
+        })
+        
+        let foundBorderLayer: CAShapeLayer?
+        
+        // Remove border layer when border is zero
+        if borderWidth == 0 {
+            if let foundBorderLayerIndex = foundBorderLayerIndex {
+                self.layer.sublayers?.remove(at: foundBorderLayerIndex)
+            }
+        } else {
+            
+            if let foundBorderLayerIndex = foundBorderLayerIndex {
+                foundBorderLayer = self.layer.sublayers?[foundBorderLayerIndex] as? CAShapeLayer
+            } else {
+                foundBorderLayer = nil
+            }
+            
+            let borderLayer: CAShapeLayer
+            
+            // Reuse existing border to avoid creating a new one
+            if let foundBorderLayer = foundBorderLayer {
+                borderLayer = foundBorderLayer
+            } else {
+                borderLayer = CAShapeLayer()
+                borderLayer.name = name
+                self.layer.addSublayer(borderLayer)
+            }
+            
+            // Bring border layer at the top
+            if bringToFront, let sublayers = self.layer.sublayers, sublayers.count > 1, let lastLayer = sublayers.last, lastLayer != borderLayer {
+                borderLayer.removeFromSuperlayer()
+                self.layer.addSublayer(borderLayer)
+            }
+            
+            borderLayer.frame = self.bounds
+            borderLayer.path = path
+            borderLayer.lineWidth = borderWidth
+            borderLayer.strokeColor = borderColor.cgColor
+            borderLayer.fillColor = UIColor.clear.cgColor
+        }
+    }
 }
