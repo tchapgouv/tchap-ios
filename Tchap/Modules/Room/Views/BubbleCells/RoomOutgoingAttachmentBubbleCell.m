c1eb879b5ce296bbd1801394c8cc2b711d595d46
giomfo <giom@matrix.org>
2018-12-23 20:55:31 +0100

    Room history: update bubbles display (#129)
    * Room history: update bubbles display
    
    -  display the all room state events and the call events like the membership events
    
    * Room history: update bubbles display
    
    - Remove the domain from the sender display name
    - Apply the right color on sender display name

--- Tchap/Modules/Room/Views/BubbleCells/RoomOutgoingAttachmentBubbleCell.m
+++ Tchap/Modules/Room/Views/BubbleCells/RoomOutgoingAttachmentBubbleCell.m
@@ -18,6 +18,7 @@
 #import "RoomOutgoingAttachmentBubbleCell.h"
 
 #import "RiotDesignValues.h"
+#import "DesignValues.h"
 
 @implementation RoomOutgoingAttachmentBubbleCell
 
@@ -25,7 +26,7 @@
 {
     [super customizeTableViewCellRendering];
     
-    self.userNameLabel.textColor = kRiotPrimaryTextColor;
+    self.userNameLabel.textColor = kColorLightNavy;
     self.messageTextView.tintColor = kRiotColorGreen;
 }
 
