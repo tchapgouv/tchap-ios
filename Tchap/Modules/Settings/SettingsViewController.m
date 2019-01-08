3faa4362a7ff5f0751116e79b2663a2d19cf9b86
Giom Foret <giom@matrix.org>
2019-01-08 15:08:00 +0100

    SettingsViewController: fix a retain cycle

--- Tchap/Modules/Settings/SettingsViewController.m
+++ Tchap/Modules/Settings/SettingsViewController.m
@@ -154,9 +154,6 @@ typedef void (^blockSettingsViewController_onReadyToDestroy)(void);
     NSMutableArray<MXDevice *> *devicesArray;
     DeviceView *deviceView;
     
-    // Observe kAppDelegateDidTapStatusBarNotification to handle tap on clock status bar.
-    id kAppDelegateDidTapStatusBarNotificationObserver;
-    
     //
     UIAlertController *resetPwdAlertController;
 
@@ -180,10 +177,11 @@ typedef void (^blockSettingsViewController_onReadyToDestroy)(void);
 @property (strong, nonatomic) id<Style> currentStyle;
 
 // Observer
-@property (nonatomic, weak) id kRiotDesignValuesDidChangeThemeNotificationObserver;
+@property (nonatomic, weak) id riotDesignValuesDidChangeThemeNotificationObserver;
 @property (nonatomic, weak) id removedAccountObserver;
 @property (nonatomic, weak) id accountUserInfoObserver;
 @property (nonatomic, weak) id pushInfoUpdateObserver;
+@property (nonatomic, weak) id appDelegateDidTapStatusBarNotificationObserver;
 
 @end
 
@@ -239,7 +237,7 @@ typedef void (^blockSettingsViewController_onReadyToDestroy)(void);
     
     // Observe user interface theme change.
     MXWeakify(self);
-    _kRiotDesignValuesDidChangeThemeNotificationObserver = [[NSNotificationCenter defaultCenter] addObserverForName:kRiotDesignValuesDidChangeThemeNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
+    _riotDesignValuesDidChangeThemeNotificationObserver = [[NSNotificationCenter defaultCenter] addObserverForName:kRiotDesignValuesDidChangeThemeNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
         
         MXStrongifyAndReturnIfNil(self);
         [self userInterfaceThemeDidChange];
@@ -328,9 +326,9 @@ typedef void (^blockSettingsViewController_onReadyToDestroy)(void);
         documentInteractionController = nil;
     }
     
-    if (_kRiotDesignValuesDidChangeThemeNotificationObserver)
+    if (_riotDesignValuesDidChangeThemeNotificationObserver)
     {
-        [[NSNotificationCenter defaultCenter] removeObserver:_kRiotDesignValuesDidChangeThemeNotificationObserver];
+        [[NSNotificationCenter defaultCenter] removeObserver:_riotDesignValuesDidChangeThemeNotificationObserver];
     }
     
     if (_removedAccountObserver)
@@ -346,6 +344,11 @@ typedef void (^blockSettingsViewController_onReadyToDestroy)(void);
     if (_pushInfoUpdateObserver)
     {
         [[NSNotificationCenter defaultCenter] removeObserver:_pushInfoUpdateObserver];
+    }
+    
+    if (_appDelegateDidTapStatusBarNotificationObserver)
+    {
+        [[NSNotificationCenter defaultCenter] removeObserver:_appDelegateDidTapStatusBarNotificationObserver];
     }
     
     [[NSNotificationCenter defaultCenter] removeObserver:self];
@@ -395,9 +398,11 @@ typedef void (^blockSettingsViewController_onReadyToDestroy)(void);
     // Refresh devices in parallel
     [self loadDevices];
     
-    // Observe kAppDelegateDidTapStatusBarNotificationObserver.
-    kAppDelegateDidTapStatusBarNotificationObserver = [[NSNotificationCenter defaultCenter] addObserverForName:kAppDelegateDidTapStatusBarNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
+    // Observe kAppDelegateDidTapStatusBarNotification.
+    MXWeakify(self);
+    _appDelegateDidTapStatusBarNotificationObserver = [[NSNotificationCenter defaultCenter] addObserverForName:kAppDelegateDidTapStatusBarNotification object:nil queue:[NSOperationQueue mainQueue] usingBlock:^(NSNotification *notif) {
         
+        MXStrongifyAndReturnIfNil(self);
         [self.tableView setContentOffset:CGPointMake(-self.tableView.mxk_adjustedContentInset.left, -self.tableView.mxk_adjustedContentInset.top) animated:YES];
         
     }];
@@ -424,10 +429,9 @@ typedef void (^blockSettingsViewController_onReadyToDestroy)(void);
         resetPwdAlertController = nil;
     }
     
-    if (kAppDelegateDidTapStatusBarNotificationObserver)
+    if (_appDelegateDidTapStatusBarNotificationObserver)
     {
-        [[NSNotificationCenter defaultCenter] removeObserver:kAppDelegateDidTapStatusBarNotificationObserver];
-        kAppDelegateDidTapStatusBarNotificationObserver = nil;
+        [[NSNotificationCenter defaultCenter] removeObserver:_appDelegateDidTapStatusBarNotificationObserver];
     }
 }
 
