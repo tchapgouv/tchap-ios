22b9069360ae26fdad221d882b11a5f986612404
giomfo <giom@matrix.org>
2019-02-20 21:47:17 +0100

    Bug Fix: Push Notification: Tchap is not opened on the right room (#161)
    - AppCoordinatorType: add showRoom() method
    - LegacyAppDelegate: remove the useless methods related to the room handling.
    - LegacyAppDelegate: remove the InAppNotification mechanism (unused in Tchap).
    - CallViewController: go back to the related chat room is disabled FTM
    
    #150

--- Tchap/Modules/Application/AppCoordinatorType.swift
+++ Tchap/Modules/Application/AppCoordinatorType.swift
@@ -26,4 +26,19 @@ protocol AppCoordinatorType: Coordinator {
     ///   - application: The shared app object that controls and coordinates your app.
     /// - Returns: true to indicate that the activity has been handled, or false to let iOS handle the activity.
     func handleUserActivity(_ userActivity: NSUserActivity, application: UIApplication) -> Bool
+    
+    /// Open a Tchap room
+    ///
+    /// - Parameters:
+    ///   - roomIdOrAlias: the room identifier.
+    ///   - eventID: an optional event identifier to point to in the room history.
+    /// - Returns: true to indicate that the room has been opened, or false if the room has not been found.
+    func showRoom(with roomIdOrAlias: String, onEventID eventID: String?) -> Bool
+}
+
+// `AppCoordinatorType` default implementation
+extension AppCoordinatorType {
+    func showRoom(with roomIdOrAlias: String) -> Bool {
+        return showRoom(with: roomIdOrAlias, onEventID: nil)
+    }
 }
