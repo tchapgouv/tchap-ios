4da303b432d9cf26d891f66af2bf343100de606d
Giom Foret <giom@matrix.org>
2020-07-06 22:39:33 +0200

    Fix an issue on the search bar display on iOS13

--- Tchap/Modules/Contacts/ContactsViewController.m
+++ Tchap/Modules/Contacts/ContactsViewController.m
@@ -139,7 +139,13 @@ NSString *const ContactErrorDomain = @"ContactErrorDomain";
     UISearchBar *searchBar = self.searchController.searchBar;
     if (searchBar)
     {
-        if (@available(iOS 11.0, *))
+        if (@available(iOS 13.0, *))
+        {
+            // iOS 13 issue: When the search bar is shown, the navigation bar color is replaced with the background color of the TableView
+            // Patch: Always show the search bar on iOS 13
+            self.navigationItem.hidesSearchBarWhenScrolling = NO;
+        }
+        else
         {
             // Enable to hide search bar on scrolling after first time view appear
             self.navigationItem.hidesSearchBarWhenScrolling = YES;
@@ -223,16 +229,9 @@ NSString *const ContactErrorDomain = @"ContactErrorDomain";
     searchController.searchBar.placeholder = NSLocalizedStringFromTable(@"contacts_search_bar_placeholder", @"Tchap", nil);
     searchController.hidesNavigationBarDuringPresentation = NO;
     
-    if (@available(iOS 11.0, *))
-    {
-        self.navigationItem.searchController = searchController;
-        // Make the search bar visible on first view appearance
-        self.navigationItem.hidesSearchBarWhenScrolling = NO;
-    }
-    else
-    {
-        self.tableView.tableHeaderView = searchController.searchBar;
-    }
+    self.navigationItem.searchController = searchController;
+    // Make the search bar visible on first view appearance
+    self.navigationItem.hidesSearchBarWhenScrolling = NO;
     
     self.definesPresentationContext = YES;
     
@@ -384,14 +383,7 @@ NSString *const ContactErrorDomain = @"ContactErrorDomain";
     
     if (searchBar)
     {
-        if (@available(iOS 11.0, *))
-        {
-            searchBar.tintColor = style.barActionColor;
-        }
-        else
-        {
-            searchBar.tintColor = style.primarySubTextColor;
-        }
+        searchBar.tintColor = style.barActionColor;
         
         UITextField *searchBarTextField = searchBar.vc_searchTextField;
         searchBarTextField.textColor = searchBar.tintColor;
