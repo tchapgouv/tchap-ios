14d30a618cb3e31ffad87f8bff2465099b5acab3
Giom Foret <giom@matrix.org>
2020-08-19 18:29:21 +0200

    Bug Fix: Join a federated room failed
    Use the canonical alias instead of the deprecated aliases array to join a public room

--- Tchap/Modules/RoomPreview/RoomPreviewCoordinator.swift
+++ Tchap/Modules/RoomPreview/RoomPreviewCoordinator.swift
@@ -94,7 +94,9 @@ final class RoomPreviewCoordinator: NSObject, RoomPreviewCoordinatorType {
         let signURL: URL?
         
         // We promote here join by room alias instead of room id when an alias is available, in order to handle federated room.
-        if let firstRoomAlias = roomPreviewData.roomAliases?.first {
+        if let canonicalAlias = roomPreviewData.roomCanonicalAlias {
+            roomIdOrAlias = canonicalAlias
+        } else if let firstRoomAlias = roomPreviewData.roomAliases?.first {
             roomIdOrAlias = firstRoomAlias
         } else {
             roomIdOrAlias = roomPreviewData.roomId
