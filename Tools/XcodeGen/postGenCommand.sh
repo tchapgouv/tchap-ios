83ce2ba02291ef9c092535570e1b147b38e0ec1d
Philippe Loriaux <philippel@element.io>
2021-10-26 09:53:35 +0200

    Fix postGenCommand script fot Tchap

--- Tools/XcodeGen/postGenCommand.sh
+++ Tools/XcodeGen/postGenCommand.sh
@@ -3,4 +3,4 @@
 # This script is invoked by xcodegen for running post commands
 
 # Move file header template in project shared data folder
-cp IDETemplateMacros.plist Riot.xcodeproj/xcshareddata/
+cp IDETemplateMacros.plist Tchap.xcodeproj/xcshareddata/
