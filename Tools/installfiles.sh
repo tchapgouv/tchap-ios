5a5598a61a18baed0703b73ea5099fcf89aabc77
Philippe Loriaux <philippel@element.io>
2021-11-10 14:33:48 +0100

    Fix Provisionning profiles and identifiers

--- Tools/installfiles.sh
+++ Tools/installfiles.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 
 installFile() {
-	echo "installfiles - Copying $2/$1 to RiotNSE/$1"
-	cp ../$2/$1 "../RiotNSE/$1"
+	echo "installfiles - Copying $2/$1 to $3/$1"
+	cp ../$2/$1 "../$3/$1"
 }
 
 cd $(dirname $0)
 echo "installfiles for : $PRODUCT_MODULE_NAME"
-installFile "Common.xcconfig" $PRODUCT_MODULE_NAME"NSE"
-installFile "BuildSettings.swift" $PRODUCT_MODULE_NAME"/Config"
+
+echo "install NSE target files"
+installFile "Common.xcconfig" $PRODUCT_MODULE_NAME"NSE" "RiotNSE"
+installFile "BuildSettings.swift" $PRODUCT_MODULE_NAME"/Config" "RiotNSE"
+installFile "AppIdentifiers.xcconfig" $PRODUCT_MODULE_NAME"/Config" "/Config"
+
 cd -
