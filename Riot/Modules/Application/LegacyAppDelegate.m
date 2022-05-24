0e0e9fe7311db892c811109bcf13a96c594a161d
Philippe Loriaux <philippel@element.io>
2022-05-24 09:30:15 +0200

    [DM] Unable to start a new DM with a contact who left the previous DM (#535)

--- Riot/Modules/Application/LegacyAppDelegate.m
+++ Riot/Modules/Application/LegacyAppDelegate.m
@@ -3316,29 +3316,65 @@ NSString *const AppDelegateUniversalLinkDidChangeNotification = @"AppDelegateUni
         if (mxSession)
         {
             MXRoom *directRoom = [mxSession directJoinedRoomWithUserId:userId];
-            
-            // if the room exists
-            if (directRoom)
-            {
-                // open it
-                Analytics.shared.viewRoomTrigger = AnalyticsViewRoomTriggerCreated;
-                [self showRoom:directRoom.roomId andEventId:nil withMatrixSession:mxSession];
-                
-                if (completion)
-                {
-                    completion();
+
+            // if the room exists and the user has not left it.
+            [self checkDirectRoomValidity:directRoom
+                                forUserID:userId
+                               completion:^(BOOL hasValidDirectRoom) {
+                if (hasValidDirectRoom) {
+                    // open it
+                    Analytics.shared.viewRoomTrigger = AnalyticsViewRoomTriggerCreated;
+                    [self showRoom:directRoom.roomId andEventId:nil withMatrixSession:mxSession];
+
+                    if (completion)
+                    {
+                        completion();
+                    }
+                } else {
+                    [self createDirectChatWithUserId:userId completion:completion];
                 }
-            }
-            else
-            {
-                [self createDirectChatWithUserId:userId completion:completion];
-            }
+            }];
         }
         else if (completion)
         {
             completion();
         }
         
+    }];
+}
+
+- (void)checkDirectRoomValidity:(MXRoom *)room
+                      forUserID:(NSString *)userID
+                     completion:(void (^)(BOOL hasValidDirectRoom))completion {
+    // Check if there is a room in parameters.
+    if (room == nil) {
+        MXLogDebug(@"[MXKRoomDataSource] checkDirectRoomLeft: No room in parameter");
+        completion(FALSE);
+    }
+    
+    [room members:^(MXRoomMembers *roomMembers) {
+        MXLogDebug(@"[MXKRoomDataSource] checkDirectRoomLeft: All room members have been retrieved");
+        BOOL hasValidDirectRoom = FALSE;
+        
+        // If room members are not 2, direct room is not valid.
+        if (roomMembers.members.count != 2) {
+            MXLogDebug(@"[MXKRoomDataSource] checkDirectRoomLeft: roomMembers.members.count != 2");
+            completion(FALSE);
+            return;
+        }
+        
+        // Check if the requested member's membership is Invite or Join.
+        for (MXRoomMember *member in roomMembers.members) {
+            if ([member.userId isEqualToString:userID] &&
+                (member.membership == MXMembershipInvite ||
+                 member.membership == MXMembershipJoin)) {
+                hasValidDirectRoom = TRUE;
+            }
+        }
+        completion(hasValidDirectRoom);
+    } failure:^(NSError *error) {
+        MXLogDebug(@"[AppDelegate] checkDirectRoomLeft: error: %@", error);
+        completion(FALSE);
     }];
 }
 
