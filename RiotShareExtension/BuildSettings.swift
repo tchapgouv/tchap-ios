8b71b7e9d47e44b61cbf4dabe23d9cdaeb37a91b
Philippe Loriaux <philippel@element.io>
2022-11-17 14:43:02 +0100

    Fix Unit tests issue with missing onboardingEnableNewAuthenticationFlow value

--- RiotShareExtension/BuildSettings.swift
+++ RiotShareExtension/BuildSettings.swift
@@ -403,6 +403,7 @@ final class BuildSettings: NSObject {
     
     // MARK: - Onboarding
     static let onboardingShowAccountPersonalization = false
+    static let onboardingEnableNewAuthenticationFlow = true
     
     // MARK: - Unified Search
     static let unifiedSearchScreenShowPublicDirectory = true
