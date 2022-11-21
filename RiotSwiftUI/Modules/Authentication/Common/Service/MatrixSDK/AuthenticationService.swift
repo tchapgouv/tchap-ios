4d9a39e6eeb108f9da9881253b6c0f1e7ba9aa20
Philippe Loriaux <philippel@element.io>
2022-11-21 10:11:03 +0100

    Fix unrecognized certificates issue

--- RiotSwiftUI/Modules/Authentication/Common/Service/MatrixSDK/AuthenticationService.swift
+++ RiotSwiftUI/Modules/Authentication/Common/Service/MatrixSDK/AuthenticationService.swift
@@ -255,21 +255,22 @@ class AuthenticationService: NSObject {
                 identityServerURL = baseURL
             }
         }
-        
-        let client = clientType.init(homeServer: homeserverURL, unrecognizedCertificateHandler: { [weak self] certificate in
+
+        // Tchap: Don't allow unrecognized certificates !
+        let client = clientType.init(homeServer: homeserverURL, unrecognizedCertificateHandler: nil) /*{ [weak self] certificate in
             guard let self = self else { return false }
-            
+
             var isTrusted = false
             let semaphore = DispatchSemaphore(value: 0)
-            
+
             self.delegate?.authenticationService(self, needsPromptFor: certificate) { didTrust in
                 isTrusted = didTrust
                 semaphore.signal()
             }
-            
+
             semaphore.wait()
             return isTrusted
-        })
+        })*/
         
         if let identityServerURL = identityServerURL {
             client.identityServer = identityServerURL.absoluteString
