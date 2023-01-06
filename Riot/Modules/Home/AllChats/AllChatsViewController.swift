49f0b7189a658574a8aac8ae2909c37402bf9445
Philippe Loriaux <philippel@element.io>
2023-01-06 14:01:55 +0100

    Fix AllChats update UI after login

--- Riot/Modules/Home/AllChats/AllChatsViewController.swift
+++ Riot/Modules/Home/AllChats/AllChatsViewController.swift
@@ -156,8 +156,6 @@ class AllChatsViewController: HomeViewController {
 
         NotificationCenter.default.addObserver(self, selector: #selector(self.spaceListDidChange), name: MXSpaceService.didInitialise, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(self.spaceListDidChange), name: MXSpaceService.didBuildSpaceGraph, object: nil)
-        // Tchap: Register user sessions service notifications to manage external users restrictions.
-        registerUserSessionsServiceNotifications()
         
         set(tableHeadeView: self.bannerView)
     }
@@ -202,12 +200,6 @@ class AllChatsViewController: HomeViewController {
         }
     }
     
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        // Tchap: Unregister user sessions service notifications to manage external users restrictions.
-        unregisterUserSessionsServiceNotifications()
-    }
-    
     // MARK: - Public
     
     func switchSpace(withId spaceId: String?) {
@@ -246,6 +238,9 @@ class AllChatsViewController: HomeViewController {
         } else {
             initDataSource()
         }
+        
+        // Tchap: Force update UI to manage external users restrictions (useful after login).
+        updateUI()
     }
     
     override func removeMatrixSession(_ mxSession: MXSession!) {
@@ -679,23 +674,6 @@ class AllChatsViewController: HomeViewController {
         
         allChatsOnboardingCoordinatorBridgePresenter.present(from: self, animated: true)
         self.allChatsOnboardingCoordinatorBridgePresenter = allChatsOnboardingCoordinatorBridgePresenter
-    }
-    
-    // Tchap: Register and unregister sessions service notifications to manage external users restrictions.
-    private func registerUserSessionsServiceNotifications() {
-        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateUserSession), name: UserSessionsService.didAddUserSession, object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateUserSession), name: UserSessionsService.willRemoveUserSession, object: nil)
-    }
-    
-    private func unregisterUserSessionsServiceNotifications() {
-        NotificationCenter.default.removeObserver(self, name: UserSessionsService.didAddUserSession, object: nil)
-        
-        NotificationCenter.default.removeObserver(self, name: UserSessionsService.willRemoveUserSession, object: nil)
-    }
-    
-    @objc private func didUpdateUserSession() {
-        updateUI()
     }
 }
 
