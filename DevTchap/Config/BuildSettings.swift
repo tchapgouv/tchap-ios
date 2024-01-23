fa8ba86f3facf32a9e13c734809afff2fe96468b
Nicolas Buquet <nbuquet@buquet-net.com>
2024-01-23 16:19:06 +0100

    Refactor Feature Flag variables names

--- DevTchap/Config/BuildSettings.swift
+++ DevTchap/Config/BuildSettings.swift
@@ -238,7 +238,7 @@ final class BuildSettings: NSObject {
     static let tchapFeatureNotificationByEmail = "tchapFeatureNotificationByEmail"
     static let tchapFeatureVoiceOverIP = "tchapFeatureVoiceOverIP"
     static let tchapFeatureVideoOverIP = "tchapFeatureVideoOverIP"    // Tchap: in Dev, allow any feature to any instance.
-    static var tchapFeatureByHomeServer: [String: [String]] = [
+    static var tchapFeaturesAllowedHomeServersForFeature: [String: [String]] = [
         tchapFeatureAnyFeature: [ tchapFeatureAnyHomeServer ]
     ]
 
