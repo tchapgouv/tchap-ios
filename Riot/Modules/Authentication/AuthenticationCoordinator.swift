c6a848feac29b4caab07f450f4ef9299796cc9f7
Nicolas Buquet <github@buquet-net.com>
2025-10-09 18:12:46 +0200

    Revert "Catch and Redirect Legacy Registration on MAS-only homeServer"
    This reverts commit 18cefea3a8fefd492e28b1dc9c54f57770da27eb.

--- Riot/Modules/Authentication/AuthenticationCoordinator.swift
+++ Riot/Modules/Authentication/AuthenticationCoordinator.swift
@@ -425,24 +425,7 @@ final class AuthenticationCoordinator: NSObject, AuthenticationCoordinatorProtoc
         MXLog.debug("[AuthenticationCoordinator] TchapShowVerifyEmailScreen")
         
         // Call `startFlow` here to get `registrationWizard` initialized.
-        do {
-            try await authenticationService.startFlow(.register)
-        }
-        catch RegistrationError.registrationDisabled {
-            // If Registration is disabled on this homeServer and SSO (MAS) is available,
-            // redirect to MAS UI.
-            TchapAuthenticationHelper.RedirectToSSO(for: "") { ssoProvider in
-                if let provider = ssoProvider {
-                    Task { @MainActor in
-                        self.presentSSOAuthentication(for: provider)
-                    }
-                }
-            }
-            return
-        }
-        catch {
-            
-        }
+        try? await authenticationService.startFlow(.register)
         
         guard let registrationWizard = authenticationService.registrationWizard else {
             MXLog.failure("[AuthenticationCoordinator] showStage: Missing the RegistrationWizard needed to complete the stage.")
