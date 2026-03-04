ab53de3770ab8f86d4262ff9161052d44813c032
Nicolas Buquet <github@buquet-net.com>
2026-03-04 20:28:26 +0100

    Add assets (strings and images)

---
+++ Tchap/Modules/Application/MigrateToNewTchap/MigrateToNewTchapView.swift
@@ -0,0 +1,18 @@
+// 
+// Copyright 2026 New Vector Ltd
+//
+// SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Element-Commercial
+// Please see LICENSE files in the repository root for full details.
+//
+
+import SwiftUI
+
+struct MigrateToNewTchapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MigrateToNewTchapView()
+}
