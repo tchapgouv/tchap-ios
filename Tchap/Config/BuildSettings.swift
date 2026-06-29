c25811e51abcc7baeaaf4741fdabdbf05360b87e
Anais Payet <pro.anaispayet@gmail.com>
2026-06-29 11:56:14 +0200

    Update unableToDecryptFaqArticleUrlString url

--- Tchap/Config/BuildSettings.swift
+++ Tchap/Config/BuildSettings.swift
@@ -140,7 +140,7 @@ final class BuildSettings: NSObject {
     static let proConnectInfoUrlString = "https://proconnect.gouv.fr/"
     static let proConnectAvailabilityFaqArticleUrlString = "https://aide.tchap.numerique.gouv.fr/fr/article/se-connecter-a-tchap-avec-proconnect-1dh1peg"
     static let signoutAlertFaqArticleUrlString = "https://aide.tchap.numerique.gouv.fr/fr/article/dechiffrement-en-cours-mes-messages-restent-verrouilles-atnp15/"
-    static let unableToDecryptFaqArticleUrlString = "https://aide.tchap.numerique.gouv.fr/fr/article/dechiffrement-impossible-de-mes-messages-comment-y-remedier-iphone-xotgv1"
+    static let unableToDecryptFaqArticleUrlString = "https://aide.tchap.numerique.gouv.fr/fr/article/dechiffrement-en-cours-mes-messages-restent-verrouilles-atnp15/"
     static let emailNotificationFaqArticleUrlString = "https://aide.tchap.numerique.gouv.fr/fr/article/notification-par-email-draft-6k7k8"
     static let newDeviceVerificationFaqArticleUrlString = "https://aide.tchap.numerique.gouv.fr/fr/article/comment-verifier-un-nouvel-appareil-sur-tchap-xm0b0y"
     
